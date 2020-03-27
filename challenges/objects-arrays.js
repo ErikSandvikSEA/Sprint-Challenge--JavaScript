16046e7c566bf0589453790df4df2438cad66ef9
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-03-27 08:51:49 -0700

    Arrays Section Complete

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -14,7 +14,7 @@ const dinoOne = {
   weight: 7000,
   length: 12,
   period: 'Late Cretaceous',
-  roar: function(){
+  roar: function () {
     return "RAWERSRARARWERSARARARRRR!"
   }
 }
@@ -78,7 +78,11 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, log the result. */
-const universities = [];
+
+const universities = graduates.map((graduate) => {
+  return graduate.university
+})
+
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -87,11 +91,20 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
-const contactInfo = [];
+const contactInfo = graduates.map((graduate) => {
+  return `${graduate.first_name} ${graduate.email}`
+});
+
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
+graduates.forEach((graduate) => {
+  if (graduate.university.includes('Uni') === true) {
+    unisWithUni.push(graduate.university)
+  }
+});
+
 console.log(unisWithUni);
 
 
